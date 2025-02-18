d427e9910cddda7dadc26a4a561e2370665f8dca
Ryan Fitz-Gerald <ztif33@gmail.com>
2025-02-18 10:39:54 -0500

    Reworked ElevArmControl to use command sequences to coordinate arm movement

--- src/main/java/frc/robot/subsystems/ElevArmControl.java
+++ src/main/java/frc/robot/subsystems/ElevArmControl.java
@@ -1,132 +1,115 @@
 package frc.robot.subsystems;
 
-import org.opencv.core.RotatedRect;
-
-import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.ParallelRaceGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class ElevArmControl extends SubsystemBase{
-    private final Arm arm;
-    private final Elevator elevator;
-    private Command holdCommand;
-    
-    private double elevTopLimit;
-    private double elevBottomLimit;
-    private Rotation2d armTopLimit;
-    private Rotation2d armBottomLimit;
+    private static ElevArmControl instance = null;
 
-    public class setElevArmCommand extends Command{
-        double elevatorPos;
-        Rotation2d armRot;
-        double elevClearance;
+    public final Command gotoIntake;    /**< Command sequence to move to intake position */
+    public final Command gotoL1;        /**< Command sequence to move to the L1 scoring position */
+    public final Command gotoL2;        /**< Command sequence to move to the L2 scoring position */
+    public final Command gotoL3;        /**< Command sequence to move to the L3 scoring position */
+    public final Command gotoL4;        /**< Command sequence to move to the L4 scoring position */
+    public final Command gotoLowAlgae;  /**< Command sequence to move to the low algae removal position */
+    public final Command gotoHighAlgae; /**< Command sequence to move to the high algae removal position   */   
 
-        public setElevArmCommand(double elevatorPos, Rotation2d armRot, double elevClearance){
-            this.elevatorPos = elevatorPos;
-            this.armRot = armRot;
-            this.elevClearance = elevClearance;
-        }
+    /**
+     * Constructor
+     */
+    private ElevArmControl(){
+        Arm arm = Arm.getInstance();
+        Elevator elev = Elevator.getInstance();
 
-        public void setPositions(double elevatorPos, Rotation2d armRot, double elevClearance){
-            this.elevatorPos = elevatorPos;
-            this. armRot = armRot;
-            this.elevClearance = elevClearance;
-        }
+        gotoIntake = new SequentialCommandGroup(
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armTravelAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorPosCommand(Constants.elevIntakePos)),
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armIntakeAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorHoldCommand())
+        );
 
-        @Override
-        public void initialize(){
-            setElevator(elevatorPos);
-        }
-        
-        @Override
-        public void execute(){
-            setArm(armRot, elevClearance);
-            
-            setElevLimit(elevBottomLimit, elevatorPos, true);
-            setElevLimit(elevTopLimit, elevatorPos, false);
-            setArmLimit(armBottomLimit, armRot, true);
-            setArmLimit(armTopLimit, armRot, false);
-        }
+        gotoL1 = new SequentialCommandGroup(
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armTravelAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorPosCommand(Constants.elevL1Pos)),
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armCoralScoreAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorHoldCommand())
+        );
+
+        gotoL2 = new SequentialCommandGroup(
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armTravelAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorPosCommand(Constants.elevL2Pos)),
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armCoralScoreAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorHoldCommand())
+        );
+
+        gotoL3 = new SequentialCommandGroup(
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armTravelAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorPosCommand(Constants.elevL3Pos)),
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armCoralScoreAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorHoldCommand())
+        );
+
+        gotoL4 = new SequentialCommandGroup(
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armTravelAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorPosCommand(Constants.elevL3Pos)),
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armCoralScoreAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorHoldCommand())
+        );
+
+        gotoLowAlgae = new SequentialCommandGroup(
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armTravelAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorPosCommand(Constants.elevLowAlgaePos)),
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armAlgaeRemoveAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorHoldCommand())
+        );
+
+        gotoHighAlgae = new SequentialCommandGroup(
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armTravelAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorPosCommand(Constants.elevHighAlgaePos)),
+            new ParallelRaceGroup(arm.new ArmAngleCommand(Constants.armAlgaeRemoveAngle), elev.new ElevatorHoldCommand()),
+            new ParallelRaceGroup(arm.new ArmHoldCommand(), elev.new ElevatorHoldCommand())
+        );
     }
 
-    public class ElevArmHoldCommand extends Command{
-
+    public void gotoIntakePos() {
+        gotoIntake.schedule();
     }
 
-
-    public ElevArmControl(){
-        arm = Arm.getInstance();
-        elevator = Elevator.getInstance();
-
-        //TODO Change values to actual elevator limits
-        elevTopLimit = 0;
-        elevBottomLimit = 0;
-        armTopLimit = new Rotation2d();
-        armBottomLimit = new Rotation2d();
-
+    public void gotoL1Pos() {
+        gotoL1.schedule();
     }
 
-    public void setElevator(double elevatorPos){
-        elevator.setPosCommand(elevatorPos);
+    public void gotoL2Pos() {
+        gotoL2.schedule();
+    }
+
+    public void gotoL3Pos() {
+        gotoL3.schedule();
+    }
+
+    public void gotoL4Pos() {
+        gotoL4.schedule();
+    }
+
+    public void gotoLowAlgaePos() {
+        gotoLowAlgae.schedule();
+    }
+
+    public void gotoHighAlgaePos() {
+        gotoHighAlgae.schedule();
     }
 
     /**
-     * @param armRot
-     * The target arm rotation
-     * @param elevClearance
-     * The position the elevator has to be at for the arm to begin going to an angle
+     * Singleton Instance access
+     * @return  the instance of the ElevArmControl class
      */
-    public void setArm(Rotation2d armRot, double elevClearance){
-        if (elevator.getElevatorPos() > elevClearance){
-            arm.setAngleCommand(armRot);
-        }
-    }
+    public static ElevArmControl getInstance() {
+        if(instance == null) instance = new ElevArmControl();
 
-    public void setHoldCommand(){
-        elevator.setHoldCommand();
-        arm.setHoldCommand();
-    }
-
-
-    /**
-     * @param elevPosLim
-     * The position limit of the elevator
-     * @param targetPos
-     * The target position of the elevator, used to check if your targetPos is less than the maximum/minimum position
-     * @param lessThan
-     * true if you want to stop the elevator when the position is less than the limit
-     */
-    public void setElevLimit(double elevPosLim, double targetPos, boolean lessThan){
-        if(lessThan){
-            if (elevator.getElevatorPos() <= elevPosLim && targetPos < elevPosLim){
-                elevator.setHoldCommand();
-            }
-        }else{
-            if (elevator.getElevatorPos() >= elevPosLim && targetPos > elevPosLim){
-                elevator.setHoldCommand();
-            }
-        }
-    }
-
-    /**
-     * @param armRotLimit
-     * The rotation limit of the arm
-     * @param targetAngle
-     * The target angle of the arm, used to check if your targetAnlge is less than the maximum/minimum angle
-     * @param lessThan
-     * true if you want to stop the arm when the angle is less than the limit
-     */
-    public void setArmLimit(Rotation2d armRotLimit, Rotation2d targetAngle, boolean lessThan){
-        if(lessThan){
-            if (arm.getArmAngle().getDegrees() <= armRotLimit.getDegrees() && targetAngle.getDegrees() < armRotLimit.getDegrees()){
-                elevator.setHoldCommand();
-            }
-        }else{
-            if (arm.getArmAngle().getDegrees() >= armRotLimit.getDegrees() && targetAngle.getDegrees() > armRotLimit.getDegrees()){
-                elevator.setHoldCommand();
-            }
-        }
-    }
+        return instance;
+    } 
     
 }
