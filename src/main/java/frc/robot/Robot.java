cf1095220f576b825e031f15e987518fda73c9d2
Ryan Fitz-Gerald <ztif33@gmail.com>
2025-02-19 11:18:56 -0500

    Removed ignore camera

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -69,7 +69,6 @@ public class Robot extends TimedRobot {
 
     @Override
     public void teleopInit() {
-        drive.ignoreCamera(false);
         if (autonomousCommand != null) {
             autonomousCommand.cancel();
         }
