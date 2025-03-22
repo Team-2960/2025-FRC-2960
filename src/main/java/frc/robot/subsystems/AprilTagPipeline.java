e6ef1b555476bdacdcc75643ecd0fe3e018ff285
han-yes <hangyulheaven@gmail.com>
2025-03-22 12:16:19 -0400

    Added distance scaling on Standard Deviation Fixed Shuffleboard display tabs

--- src/main/java/frc/robot/subsystems/AprilTagPipeline.java
+++ src/main/java/frc/robot/subsystems/AprilTagPipeline.java
@@ -22,56 +22,60 @@ import org.photonvision.PhotonUtils;
  */
 public class AprilTagPipeline extends SubsystemBase {
 
-    private final AprilTagPipelineSettings settings;                /**< Pipeline Settings */
-    private final PhotonCamera camera;              /**< Camera object */
-    private final PhotonPoseEstimator pose_est;     /**< Pose Estimator */
+    private final AprilTagPipelineSettings settings;
+    /** < Pipeline Settings */
+    private final PhotonCamera camera;
+    /** < Camera object */
+    private final PhotonPoseEstimator pose_est;
+    /** < Pose Estimator */
 
-    private Pose2d last_pose;                       /**< Most recent estimated pose */
-    private double last_timestamp;                  /**< Timestamp of the most recent pose estimation */
+    private Pose2d last_pose;
+    /** < Most recent estimated pose */
+    private double last_timestamp;
+    /** < Timestamp of the most recent pose estimation */
     private double maxDistance;
     private final double ambiguity_threshold;
 
-    // Shuffleboard 
+    // Shuffleboard
     private GenericEntry sb_PoseX;
     private GenericEntry sb_PoseY;
     private GenericEntry sb_PoseR;
     private GenericEntry sb_lastTimestamp;
     private GenericEntry sb_lastUpdatePeriod;
 
-    //Camera Simulation
+    // Camera Simulation
     // VisionSystemSim visionSim;
     // TargetModel targetModel;
     // SimCameraProperties cameraProp;
     // PhotonCameraSim cameraSim;
-    //VisionTargetSim visionTargetSim;
+    // VisionTargetSim visionTargetSim;
 
-    //Test Values
+    // Test Values
     private double displayNum;
     private String resultsList;
     private boolean tagPresent;
 
     /**
      * Constructor
-     * @param   settings    Pipeline settings
+     * 
+     * @param settings Pipeline settings
      */
     public AprilTagPipeline(AprilTagPipelineSettings settings, String cameraName, String name) {
         this.settings = settings;
-       // TODO: Find non-depricated method to get April Tag Field Locations
+        // TODO: Find non-depricated method to get April Tag Field Locations
         camera = new PhotonCamera(cameraName);
         pose_est = new PhotonPoseEstimator(
-            AprilTagFieldLayout.loadField(settings.field_layout), 
-            settings.pose_strategy, 
-            settings.robot_to_camera
-        );
+                AprilTagFieldLayout.loadField(settings.field_layout),
+                settings.pose_strategy,
+                settings.robot_to_camera);
 
         last_pose = new Pose2d();
         last_timestamp = 0;
         maxDistance = settings.max_dist;
         ambiguity_threshold = settings.ambiguity_threshold;
-        
 
         // Setup Shuffleboard
-        var layout = Shuffleboard.getTab("AprilTags" + name)
+        var layout = Shuffleboard.getTab("AprilTags")
                 .getLayout(name, BuiltInLayouts.kList)
                 .withSize(1, 4);
         sb_PoseX = layout.add("Pose X" + cameraName, 0).getEntry();
@@ -80,7 +84,7 @@ public class AprilTagPipeline extends SubsystemBase {
         sb_lastTimestamp = layout.add("Last Timestamp" + cameraName, last_timestamp).getEntry();
         sb_lastUpdatePeriod = layout.add("Time Since Last Update" + cameraName, 0).getEntry();
 
-        //Vision Simulation
+        // Vision Simulation
         // visionSim = new VisionSystemSim("main");
         // visionSim.addAprilTags(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape));
         // targetModel = TargetModel.kAprilTag16h5;
@@ -88,12 +92,14 @@ public class AprilTagPipeline extends SubsystemBase {
         // cameraProp.setFPS(60);
         // cameraProp.setCalibration(640, 480, Rotation2d.fromDegrees(70));
         // cameraSim = new PhotonCameraSim(camera, cameraProp);
-        // //visionTargetSim = new VisionTargetSim(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(17).get(), targetModel);
+        // //visionTargetSim = new
+        // VisionTargetSim(AprilTagFieldLayout.loadField(AprilTagFields.k2025Reefscape).getTagPose(17).get(),
+        // targetModel);
         // visionSim.addCamera(cameraSim, settings.robot_to_camera);
         // cameraSim.enableDrawWireframe(true);
 
-        //Test Values
-        //TODO Delete after testing
+        // Test Values
+        // TODO Delete after testing
         displayNum = -1;
         resultsList = "";
         tagPresent = false;
@@ -116,56 +122,39 @@ public class AprilTagPipeline extends SubsystemBase {
         Drive drive = Drive.getInstance();
         var unreadResults = camera.getAllUnreadResults();
         // visionSim.update(new Pose2d(17.526/2, 8.05/2, new Rotation2d()));
-        
-        for(var change : unreadResults) {
+
+        for (var change : unreadResults) {
             visionEst = pose_est.update(change);
 
             // Check if a pose was estimated
-            if(visionEst.isPresent()) {
+            if (visionEst.isPresent()) {
                 double est_timestamp = change.getTimestampSeconds();
 
                 // Get found tag count and average distance
-                for(var tag : change.getTargets()) {
+                for (var tag : change.getTargets()) {
                     var tag_pose3d = pose_est.getFieldTags().getTagPose(tag.getFiducialId());
-                    
-                    if(!tag_pose3d.isEmpty()) {
-                        Pose2d tag_pose = tag_pose3d.get().toPose2d();
-                        Pose3d estPose = PhotonUtils.estimateFieldToRobotAprilTag(tag.getBestCameraToTarget(), tag_pose3d.get(), settings.robot_to_camera.inverse());
-                        if (tag.getPoseAmbiguity() <= ambiguity_threshold
-                             && tag.getBestCameraToTarget().getTranslation().getDistance(new Translation3d()) <= maxDistance){
-                                drive.addVisionPose(estPose.toPose2d(), est_timestamp, settings.multi_tag_std);
+
+                    if (!tag_pose3d.isEmpty()) {
+                        Pose3d estPose = PhotonUtils.estimateFieldToRobotAprilTag(tag.getBestCameraToTarget(),
+                                tag_pose3d.get(), settings.robot_to_camera.inverse());
+                        if (tag.getPoseAmbiguity() <= ambiguity_threshold) {
+                            double dist = tag.getBestCameraToTarget().getTranslation().getDistance(new Translation3d());
+                            if (dist <= maxDistance) {
+                                Vector<N3> est_std = settings.single_tag_std;
+                                est_std = est_std.times(1 + (dist * dist / 30));
+
+                                drive.addVisionPose(estPose.toPose2d(), est_timestamp, est_std);
+                                last_pose = estPose.toPose2d();
+                            }
                         }
                     }
-                    
+
                 }
 
-                // Check if any targets were found
-                // if(tag_count > 0) {
-                //     Vector<N3> est_std = settings.single_tag_std;
-
-                //     // Calculate average target distance
-                //     avg_dist /= tag_count;
-
-                //     // Decrease standard deviation if multiple tags are found
-                //     if(tag_count > 1) est_std = settings.multi_tag_std;
-
-                //     // Increase standard deviation based on average distance and ignore single 
-                //     // target results over the settings.max_dist
-                //     if(tag_count > 1 || avg_dist < settings.max_dist) {
-                //         // TODO Add average distance scaler to settings
-                //         est_std = est_std.times(1 + (avg_dist * avg_dist / 30));
-                        
-                //         last_pose = est_pose;
-                //         last_timestamp = est_timestamp;
-                //         drive.addVisionPose(est_pose, est_timestamp, est_std);
-                //     }
-                    
-                // }
             }
-        
+
         }
     }
-    
 
     /**
      * Updates Shuffleboard
