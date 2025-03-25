0fe8d4196469b5c328039c3a62acbcb6f5b44c30
han-yes <hangyulheaven@gmail.com>
2025-03-24 21:52:08 -0400

    Created New NamedCommands for Auto Align Overriding Adjusted Autons and Paths in an attempt to work with PathPlanner

--- src/main/java/frc/robot/Auton/RobotContainer.java
+++ src/main/java/frc/robot/Auton/RobotContainer.java
@@ -2,20 +2,29 @@ package frc.robot.Auton;
 
 import com.pathplanner.lib.auto.AutoBuilder;
 import com.pathplanner.lib.auto.NamedCommands;
+import com.pathplanner.lib.controllers.PPHolonomicDriveController;
 
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+import frc.robot.Constants;
 import frc.robot.subsystems.Arm;
 import frc.robot.subsystems.Drive;
+import frc.robot.subsystems.Drive.LinearDriveCommands.LinearGoToReefCommand;
 import frc.robot.subsystems.ElevArmControl;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.EndEffector;
+
 public class RobotContainer {
   SendableChooser<Command> autoChooser;
   Command chosenAuto;
+  Command leftBranchAlign;
+  Command rightBranchAlign;
   private static RobotContainer robotContainer = null;
 
   public RobotContainer(){
@@ -25,7 +34,42 @@ public class RobotContainer {
     Arm arm = Arm.getInstance();
     EndEffector endEffector = EndEffector.getInstance();
 
+    // leftBranchAlign = new ParallelCommandGroup(
+    //   drive.linearDriveCommands.new LinearGoToReefCommand(
+    //     Constants.leftBranchOffset),
+    //   drive.rotationDriveCommands
+    //     .new RotGoToReefCommand(Rotation2d.fromDegrees(0))
+    // );
+
+    // rightBranchAlign = new ParallelCommandGroup(
+    //   drive.linearDriveCommands
+    //     .new LinearGoToReefCommand(
+    //       Constants.rightBranchOffset),
+    //   drive.rotationDriveCommandsl
+    //     .new RotGoToReefCommand(Rotation2d.fromDegrees(0))
+    // );
+
+    // rightBranchAlign = new RunCommand(() -> {
+    //     PPHolonomicDriveController.overrideXFeedback(() -> drive.getCalcToPoint(drive.calcGoToReef(new Pose2d(Constants.rightBranchOffset, Rotation2d.fromDegrees(0))).getTranslation()).get(1));
+    //     PPHolonomicDriveController.overrideYFeedback(() -> drive.getCalcToPoint(drive.calcGoToReef(new Pose2d(Constants.rightBranchOffset, Rotation2d.fromDegrees(0))).getTranslation()).get(2));
+    //   }
+    //   , drive.linearDriveCommands);
+
+    rightBranchAlign = new ParallelCommandGroup(
+      drive.linearDriveCommands.new AutonLinearGoToReefCommand(Constants.rightBranchOffset),
+      drive.rotationDriveCommands.new AutonRotGoToReefCommand(Rotation2d.fromDegrees(0))
+    );
+
+    leftBranchAlign = new ParallelCommandGroup(
+      drive.linearDriveCommands.new AutonLinearGoToReefCommand(Constants.leftBranchOffset),
+      drive.rotationDriveCommands.new AutonRotGoToReefCommand(Rotation2d.fromDegrees(0))
+    );
+
+
+
     NamedCommands.registerCommand("goToIntakeCommand", elevArmControl.getGoToIntakeCommand());
+    NamedCommands.registerCommand("leftBranchAlign", leftBranchAlign);
+    NamedCommands.registerCommand("rightBranchAlign", rightBranchAlign);
     NamedCommands.registerCommand("goToL1Command", elevArmControl.getGoToL1Command());
     NamedCommands.registerCommand("goToL2Command", elevArmControl.getGoToL2Command());
     NamedCommands.registerCommand("goToL3Command", elevArmControl.getGoToL3Command());
@@ -42,6 +86,7 @@ public class RobotContainer {
     autoChooser = AutoBuilder.buildAutoChooser();
     
     SmartDashboard.putData("Choose Auto", autoChooser);
+
   }
 
   public Command getAutonomousCommand() {
